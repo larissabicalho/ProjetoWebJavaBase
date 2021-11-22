42ad03ef2bb9a375f2798b80a47c8392340b6526
larissabicalho <lari_nazarebicalho@hotmail.com>
2021-11-22 11:39:13 -0300

    comecando o projeto

---
+++ src/test/java/com/javaseleniumtemplate/tests/LoginTests.java
@@ -0,0 +1,51 @@
+package com.javaseleniumtemplate.tests;
+
+import com.javaseleniumtemplate.bases.TestBase;
+import com.javaseleniumtemplate.dbsteps.UsuariosDBSteps;
+import com.javaseleniumtemplate.pages.LoginPage;
+import com.javaseleniumtemplate.pages.MainPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase {
+    //Objects
+    LoginPage loginPage;
+    MainPage mainPage;
+
+    //Tests
+    @Test
+    public void efetuarLoginComSucesso(){
+        //Objects instances
+        loginPage = new LoginPage();
+        mainPage = new MainPage();
+
+        //Parameteres
+        String usuario = "templateautomacao";
+        String senha = "123456";
+
+        //Test
+        loginPage.preenhcerUsuario(usuario);
+        loginPage.preencherSenha(senha);
+        loginPage.clicarEmLogin();
+
+        Assert.assertEquals(usuario, mainPage.retornaUsernameDasInformacoesDeLogin());
+    }
+
+    //@Test
+    public void efetuarLoginComSucesso_SenhaRetornadaDoDB(){
+        //Objects instances
+        loginPage = new LoginPage();
+        mainPage = new MainPage();
+
+        //Parameteres
+        String usuario = "templateautomacao";
+        String senha = UsuariosDBSteps.retornaSenhaDoUsuarioDB(usuario);
+
+        //Test
+        loginPage.preenhcerUsuario(usuario);
+        loginPage.preencherSenha(senha);
+        loginPage.clicarEmLogin();
+
+        Assert.assertEquals(usuario, mainPage.retornaUsernameDasInformacoesDeLogin());
+    }
+}
